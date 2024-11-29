4de22da6e0dfe0bc47fd07dc7afeb17e6e7ae53d
3Squ4red <abhijeetranjan390@gmail.com>
2024-11-29 23:42:22 +0530

    update the token contract

---
+++ packages/foundry/contracts/Gamerge.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Gamerge is ERC20, ERC20Burnable, ERC20Permit, Ownable {
+    event SymbolUpdated(string indexed newSymbol);
+
+    string private _symbol;
+    uint256 public constant CAP = 100_000_000 * 10e18; // 100 million;
+    
+    constructor() ERC20("Gamerge", "GMG") ERC20Permit("Gamerge") Ownable(msg.sender) {
+        _symbol = "GMG";
+        _mint(msg.sender, CAP);
+    }
+
+    function setSymbol(string calldata _newSymbol) external onlyOwner {
+        _symbol = _newSymbol;
+
+        emit SymbolUpdated(_newSymbol);
+    }
+    
+    function symbol() public view override returns (string memory) {
+        return _symbol;
+    }
+}
