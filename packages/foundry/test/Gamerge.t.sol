593806657212305e5686b52b61e84b0fdd1b8907
3Squ4red <abhijeetranjan390@gmail.com>
2024-11-29 23:45:31 +0530

    add tests

---
+++ packages/foundry/test/Gamerge.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../contracts/Gamerge.sol";
+
+contract GamergeTest is Test {
+
+  address owner = makeAddr("owner");
+  Gamerge public gamerge;
+
+  function setUp() public {
+    vm.prank(owner);
+    gamerge = new Gamerge();
+  }
+
+  function testUpdatingSymbol() public {
+    string memory before = gamerge.symbol();
+
+    vm.prank(owner);
+    gamerge.setSymbol("MGM");
+
+    assertEq(gamerge.symbol(), "MGM");
+  }
+}
